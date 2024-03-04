e021453e0bb68fadc8ee3306c4a8297e9578f410
jonaspeixoto <jonas.peixoto.087@gmail.com>
2024-03-04 08:32:44 -0300

    Implementação de camada de controle com arvore hirarquica e filtragem de investimentos

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import L, { Map, latLng, tileLayer } from 'leaflet';
 import { MiniMap } from 'leaflet-control-mini-map';
 import { EstadoService } from '../../service/estado.service';
@@ -13,12 +13,15 @@ import 'leaflet.control.layers.tree';
   templateUrl: './map.component.html',
   styleUrl: './map.component.css'
 })
-export class MapComponent {
+export class MapComponent implements AfterViewInit {
   INITIAL_COORD = [-5.844865661075205, -36.56710587301696];
   baseMapURl: string = 'http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}';
   layers: L.LayerGroup = new L.LayerGroup();
   layerControl!: L.Control.Layers;
   invest: any;
+  investimentos_estrutura: any
+  ctl: any
+  vetorMaker:any = []
 
   options = {
     layers: [
@@ -41,6 +44,12 @@ export class MapComponent {
     private territorioService: TerritorioService,
     private investimentoService: InvestimentosService,
   ) { }
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.filtrarCamadasPorInput();
+    }, 5000);
+
+  }
 
   onMapReady(map: Map): void {
     this.initializeLayerControl(map);
@@ -49,11 +58,11 @@ export class MapComponent {
     this.getTerritorioLayer();
     this.getInvestimentosLayer();
     this.mapService.setMap(map);
-    this.filtrarCamadasPorInput()
+    
   }
 
   initializeLayerControl(map: Map): void {
-    var osm = L. tileLayer(this.baseMapURl, { maxZoom: 12, subdomains: ['mt0', 'mt1', 'mt2', 'mt3'], attribution: '© contributors: @joabesamuell, @_ig0rdias, @celilimaf, @rodmatth, @jonas.ssilva' })
+    var osm = L.tileLayer(this.baseMapURl, { maxZoom: 12, subdomains: ['mt0', 'mt1', 'mt2', 'mt3'], attribution: '© contributors: @joabesamuell, @_ig0rdias, @celilimaf, @rodmatth, @jonas.ssilva' })
 
     var baseTree = {
       label: 'BaseLayers',
@@ -69,15 +78,17 @@ export class MapComponent {
       ]
     };
 
-    var ctl = L.control.layers.tree(baseTree, undefined,
+    this.ctl = L.control.layers.tree(baseTree, undefined,
       {
-        namedToggle: true,
+        // namedToggle: true,
         // collapseAll: 'Collapse all',
         // expandAll: 'Expand all',
-        collapsed: false,
+        // collapsed: false,
       });
 
-    this.layerControl = ctl.addTo(map).collapseTree().expandSelected();
+    this.layerControl = this.ctl.addTo(map).collapseTree().expandSelected();
+
+    this.ctl.addTo(map).collapseTree().expandSelected();
 
   }
 
@@ -120,52 +131,132 @@ export class MapComponent {
       .subscribe(response => {
         const territorio = new L.GeoJSON(response, { style: whiteBackground })
           .addTo(this.layers);
-        this.layerControl.addOverlay(territorio, "Territórios");
+        // this.layerControl.addOverlay(territorio, "Territórios");
       });
   }
 
   getInvestimentosLayer(): void {
     this.investimentoService.findAllSlim().subscribe(
       response => {
-        this.invest = new L.GeoJSON(response, {
-          pointToLayer: FeatureUtils.setCustomMark,
-          onEachFeature: FeatureUtils.customBindPopup
-        }).addTo(this.layers);
-        this.layerControl.addOverlay(this.invest, "Investimentos");
-
+        let investimentos = response;
+        let features = (investimentos as any).features;
+        for (let i = 0; i < features.length; i++) {
+          let label = features[i].properties.investimentoMapeamento;
+          let cordenadas = features[i].geometry.coordinates;
+          let type = features[i].geometry.type;
+          let newElement;
+  
+          if (type == "Point") {
+            let myIcon = criarIconPersonalizado(features[i]);
+            const marker = L.marker([cordenadas[1], cordenadas[0]], { icon: myIcon });
+            FeatureUtils.customBindPopup(features[i], marker);
+            const objetoMarcadorFeature = {
+              marcador: marker,
+              feature: features[i]
+            };
+            this.vetorMaker.push(objetoMarcadorFeature);
+            newElement = { label: label, layer: marker };
+          }
+  
+          if (!this.investimentos_estrutura) {
+            this.investimentos_estrutura = {
+              label: 'investimentos',
+              selectAllCheckbox: true,
+              children: [
+                {
+                  label: 'desenvolvimento regional',
+                  selectAllCheckbox: true,
+                  children: [
+                    {
+                      label: 'agua',
+                      selectAllCheckbox: true,
+                      children: []
+                    },
+                  ]
+                }
+              ]
+            };
+          }
+          this.investimentos_estrutura.children[0].children[0].children.push(newElement);
+        }
+        this.ctl.setOverlayTree(this.investimentos_estrutura).collapseTree(true).expandSelected(true);
+        controlarEventosFiltragem();
       });
+  
+    function criarIconPersonalizado(feature: any) {
+      const BASE_CAMINHO_IMAGEM = 'assets/icones_novos';
+      const nameIcon = FeatureUtils.getIconPath(feature, BASE_CAMINHO_IMAGEM);
+      const myIcon = L.icon({
+        iconUrl: nameIcon,
+        iconSize: [35, 35],
+        shadowSize: [35, 20],
+        iconAnchor: [12, 12],
+        shadowAnchor: [12, 6],
+        popupAnchor: [0, 0]
+      });
+      return myIcon;
+    }
+  
+    function controlarEventosFiltragem() {
+      const parentElements: NodeListOf<HTMLElement> = document.querySelectorAll<HTMLElement>('.leaflet-layerstree-header.leaflet-layerstree-header-pointer');
+      parentElements.forEach(parentElement => {
+        // Oculta todos os filhos inicialmente
+        const children: HTMLCollection = parentElement.parentElement!.children;
+        for (let i = 0; i < children.length; i++) {
+          const child: HTMLElement = children[i] as HTMLElement;
+          if (child !== parentElement) {
+            child.style.display = 'none';
+          }
+        }
+  
+        parentElement.addEventListener('click', () => {
+          const children: HTMLCollection = parentElement.parentElement!.children;
+          for (let i = 0; i < children.length; i++) {
+            const child: HTMLElement = children[i] as HTMLElement;
+            if (child !== parentElement) {
+              if (child.style.display === 'none') {
+                child.style.display = 'block';
+              } else {
+                child.style.display = 'none';
+              }
+            }
+          }
+        });
+      });
+    }
+  
+    this.filtrarCamadasPorInput();
   }
+  
   filtrarCamadasPorInput() {
     const filterInput = document.querySelector('.filtro-pesquisa') as HTMLInputElement;
     filterInput.addEventListener('input', () => {
       const filterValue = filterInput.value.toLowerCase();
-      this.invest.eachLayer((layer: any) => {
-        if (layer._icon) {
-          layer._icon.style.display = 'None';
-          if (contem_municipio_tipologia_territorio_categoria_invest(layer, filterValue)) {
-            layer._icon.style.display = 'block';
+      for (let i = 0; i < this.vetorMaker.length; i++) {
+        if (this.vetorMaker[i]['marcador']) {
+          this.vetorMaker[i]['marcador'].dragging._marker._icon.style.display = 'none';
+          if (contemMunicipioTipologiaTerritorioCategoriaInvest(this.vetorMaker[i]['feature'], filterValue)) {
+            this.vetorMaker[i]['marcador'].dragging._marker._icon.style.display = 'block';
+          }
+        } else if (this.vetorMaker[i]._path) {
+          this.vetorMaker[i].dragging._marker._icon.display = 'none';
+          if (contemMunicipioTipologiaTerritorioCategoriaInvest(this.vetorMaker[i]['feature'], filterValue)) {
+            this.vetorMaker[i].dragging._marker._icon.display = 'block';
           }
         }
-        else if (layer._path) {
-          layer._path.style.display = 'None';
-          if (contem_municipio_tipologia_territorio_categoria_invest(layer, filterValue)) {
-            layer._path.style.display = 'block';
-          }
-        }
-      });
-
+      }
     });
-
-    function contem_municipio_tipologia_territorio_categoria_invest(layer: any, filterValue: string) {
-      const estabelecimento = layer.feature.properties.estabelecimento;
-      const municipio = layer.feature.properties.municipio
-      const territorio = layer.feature.properties.territorio
-      const tipoDeInvestimento = layer.feature.properties.tipoDeInvestimento
-      const categoriaMapeamento = layer.feature.properties.categoriaMapeamento
-
+  
+    function contemMunicipioTipologiaTerritorioCategoriaInvest(layer: any, filterValue: string) {
+      const estabelecimento = layer.properties.estabelecimento;
+      const municipio = layer.properties.municipio;
+      const territorio = layer.properties.territorio;
+      const tipoDeInvestimento = layer.properties.tipoDeInvestimento;
+      const categoriaMapeamento = layer.properties.categoriaMapeamento;
+  
       return municipio.toLowerCase().includes(filterValue) || territorio.toLowerCase().includes(filterValue)
         || tipoDeInvestimento.toLowerCase().includes(filterValue) || categoriaMapeamento.toLowerCase().includes(filterValue)
         || estabelecimento.toLowerCase().includes(filterValue);
     }
-  };
+  }  
 }
