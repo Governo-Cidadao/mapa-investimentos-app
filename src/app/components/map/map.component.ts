1ac85b434a87cc8bff865b139462cca6e6a05fa4
jonaspeixoto <jonas.peixoto.087@gmail.com>
2024-03-06 14:43:59 -0300

    #Refactor up

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -20,7 +20,7 @@ export class MapComponent implements AfterViewInit {
   layerControl!: L.Control.Layers;
   invest: any;
   investimentos_estrutura: any
-  ctl: any
+  controler: any
   vetorMaker: any = []
 
   options = {
@@ -66,12 +66,12 @@ export class MapComponent implements AfterViewInit {
       label: 'BaseLayers',
     };
 
-    this.ctl = L.control.layers.tree(baseTree, undefined, {
+    this.controler = L.control.layers.tree(baseTree, undefined, {
     });
 
-    this.layerControl = this.ctl.addTo(map).collapseTree().expandSelected();
+    this.layerControl = this.controler.addTo(map).collapseTree().expandSelected();
 
-    this.ctl.addTo(map).collapseTree().expandSelected();
+    this.controler.addTo(map).collapseTree().expandSelected();
 
   }
 
@@ -121,175 +121,50 @@ export class MapComponent implements AfterViewInit {
   getInvestimentosLayer(): void {
     this.investimentoService.findAllSlim().subscribe(
       response => {
+
         let investimentos = response;
         let features = (investimentos as any).features;
-        let investimentosMapeamento = filtarInvestimentos(features)
+        let investimentosMapeamento = this.filtarInvestimentos(features)
 
-        this.investimentos_estrutura = {
-          label: ' INVESTIMENTOS',
-          selectAllCheckbox: 'true',
-          children: [] as Array<{ label: string; selectAllCheckbox: boolean; children: Array<any> }>
-        };
-
-        
-
-
-        investimentosMapeamento.forEach((area: any) => {
-          let areaNode = {
-            label: ' ' + area.areaMapeamento,
-            selectAllCheckbox: true,
-            children: [] as Array<{ label: string; selectAllCheckbox: boolean; children: Array<any> }>
-
-          };
-
-          area.tipologias.forEach((tipologia: any) => {
-            let tipologiaNode = {
-              label: ' ' + tipologia.tipologiaMapeamento,
-              selectAllCheckbox: true,
-              children: [] as Array<{ label: string; selectAllCheckbox: boolean; children: Array<any> }>
-
-            };
-            areaNode.children.push(tipologiaNode);
-
-
-            tipologia.categorias.forEach((categoria: any) => {
-              let categoriaNode = {
-                label: ' ' + categoria.categoriaMapeamento,
-                selectAllCheckbox: true,
-                children: [] as Array<{ label: string; selectAllCheckbox: boolean; children: Array<any> }>
-
-              };
-
-              categoria.elementos.forEach((elemento: any) => {
-                let label = ' ' + elemento.properties.categoriaMapeamento
-                let cordenadas = elemento.geometry.coordinates;
-                let type = elemento.geometry.type;
-                let newElement: any;
-
-                if (type == "Point") {
-                  let myIcon = criarIconPersonalizado(elemento);
-                  const marker = L.marker([cordenadas[1], cordenadas[0]], { icon: myIcon });
-                  FeatureUtils.customBindPopup(elemento, marker);
-                  const objetoMarcadorFeature = {
-                    marcador: marker,
-                    feature: elemento
-                  };
-                  this.vetorMaker.push(objetoMarcadorFeature);
-                  newElement = { label: label, layer: marker };
-                }
-                tipologiaNode.children.push(newElement);
-
-              })
-
-            })
-          })
-          this.investimentos_estrutura.children.push(areaNode);
-        })
-        // this.ctl.setOverlayTree(this.investimentos_estrutura).collapseTree(true).expandSelected(true);
-        this.ctl.setOverlayTree(this.investimentos_estrutura)
+        // função responsavel por criar a estrutura requirida pela biblioteca layerTree para ser adicionada ao controler
+        this.criarEstruturaArvore(investimentosMapeamento)
+        this.controler.setOverlayTree(this.investimentos_estrutura)
 
         controlarEventosFiltragem();
       });
 
 
-    function filtarInvestimentos(features: any) {
-      let investimentosMapeamento: any = [];
-      for (let i = 0; i < features.length; i++) {
-        let areaIndex = investimentosMapeamento.findIndex((element: any) =>
-          element.areaMapeamento === features[i].properties.areaMapeamento
-        );
-        console.log(areaIndex)
-
-        if (areaIndex === -1) {
-          investimentosMapeamento.push({
-            areaMapeamento: features[i].properties.areaMapeamento,
-            tipologias: [{
-              tipologiaMapeamento: features[i].properties.tipologiaMapeamento,
-              categorias: [{
-                categoriaMapeamento: features[i].properties.categoriaMapeamento,
-                elementos: [features[i]]
-              }]
-            }]
-          });
-        } else {
-          let tipologiaIndex = investimentosMapeamento[areaIndex].tipologias.findIndex((element: any) =>
-            element.tipologiaMapeamento === features[i].properties.tipologiaMapeamento
-          );
-
-          if (tipologiaIndex === -1) {
-            investimentosMapeamento[areaIndex].tipologias.push({
-              tipologiaMapeamento: features[i].properties.tipologiaMapeamento,
-              categorias: [{
-                categoriaMapeamento: features[i].properties.categoriaMapeamento,
-                elementos: [features[i]]
-              }]
-            });
-          } else {
-            let categoriaIndex = investimentosMapeamento[areaIndex].tipologias[tipologiaIndex].categorias.findIndex((element: any) =>
-              element.categoriaMapeamento === features[i].properties.categoriaMapeamento
-            );
-
-            if (categoriaIndex === -1) {
-              investimentosMapeamento[areaIndex].tipologias[tipologiaIndex].categorias.push({
-                categoriaMapeamento: features[i].properties.categoriaMapeamento,
-                elementos: [features[i]]
-              });
-            } else {
-              investimentosMapeamento[areaIndex].tipologias[tipologiaIndex].categorias[categoriaIndex].elementos.push(features[i]);
-            }
-          }
-        }
-      }
-      return investimentosMapeamento
-    }
-
-    function criarIconPersonalizado(feature: any) {
-      const BASE_CAMINHO_IMAGEM = 'assets/icones_novos';
-      const nameIcon = FeatureUtils.getIconPath(feature, BASE_CAMINHO_IMAGEM);
-      const myIcon = L.icon({
-        iconUrl: nameIcon,
-        iconSize: [35, 35],
-        shadowSize: [35, 20],
-        iconAnchor: [12, 12],
-        shadowAnchor: [12, 6],
-        popupAnchor: [0, 0]
-      });
-      return myIcon;
-    }
-
     function controlarEventosFiltragem() {
-      const parentElements: NodeListOf<HTMLElement> = document.querySelectorAll<HTMLElement>('.leaflet-layerstree-header.leaflet-layerstree-header-pointer');
-      const elementoTeclado: NodeListOf<HTMLElement> | null = document.querySelectorAll<HTMLElement>(".leaflet-layerstree-children");
+      const headerController: NodeListOf<HTMLElement> = document.querySelectorAll<HTMLElement>('.leaflet-layerstree-header.leaflet-layerstree-header-pointer');
+      const filhosController: NodeListOf<HTMLElement> | null = document.querySelectorAll<HTMLElement>(".leaflet-layerstree-children");
 
-      if (elementoTeclado)
-        elementoTeclado.forEach(elemento => {
+      if (filhosController)
+        filhosController.forEach(elemento => {
           elemento.style.marginLeft = '15px'
         })
 
 
-      parentElements.forEach(elemento => {
+      headerController.forEach(elemento => {
         let span = elemento.querySelector('span')
         if (span) {
           span.style.display = 'none'
         }
-
       })
 
-
-      parentElements.forEach(parentElement => {
-        const children: HTMLCollection = parentElement.parentElement!.children;
+      headerController.forEach(elemento => {
+        const children: HTMLCollection = elemento.parentElement!.children;
         for (let i = 0; i < children.length; i++) {
           const child: HTMLElement = children[i] as HTMLElement;
-          if (child !== parentElement) {
+          if (child !== elemento) {
             child.style.display = 'none';
           }
         }
 
-        parentElement.addEventListener('click', () => {
-          const children: HTMLCollection = parentElement.parentElement!.children;
+        elemento.addEventListener('click', () => {
+          const children: HTMLCollection = elemento.parentElement!.children;
           for (let i = 0; i < children.length; i++) {
             const child: HTMLElement = children[i] as HTMLElement;
-            if (child !== parentElement) {
+            if (child !== elemento) {
               if (child.style.display === 'none') {
                 child.style.display = 'block';
               } else {
@@ -302,6 +177,129 @@ export class MapComponent implements AfterViewInit {
     }
 
     this.filtrarCamadasPorInput();
+  }
+  filtarInvestimentos(features: any) {
+    let investimentosMapeamento: any = [];
+    for (let i = 0; i < features.length; i++) {
+      let areaIndex = investimentosMapeamento.findIndex((element: any) =>
+        element.areaMapeamento === features[i].properties.areaMapeamento
+      );
+      if (areaIndex === -1) {
+        investimentosMapeamento.push({
+          areaMapeamento: features[i].properties.areaMapeamento,
+          tipologias: [{
+            tipologiaMapeamento: features[i].properties.tipologiaMapeamento,
+            categorias: [{
+              categoriaMapeamento: features[i].properties.categoriaMapeamento,
+              elementos: [features[i]]
+            }]
+          }]
+        });
+      } else {
+        let tipologiaIndex = investimentosMapeamento[areaIndex].tipologias.findIndex((element: any) =>
+          element.tipologiaMapeamento === features[i].properties.tipologiaMapeamento
+        );
+
+        if (tipologiaIndex === -1) {
+          investimentosMapeamento[areaIndex].tipologias.push({
+            tipologiaMapeamento: features[i].properties.tipologiaMapeamento,
+            categorias: [{
+              categoriaMapeamento: features[i].properties.categoriaMapeamento,
+              elementos: [features[i]]
+            }]
+          });
+        } else {
+          let categoriaIndex = investimentosMapeamento[areaIndex].tipologias[tipologiaIndex].categorias.findIndex((element: any) =>
+            element.categoriaMapeamento === features[i].properties.categoriaMapeamento
+          );
+
+          if (categoriaIndex === -1) {
+            investimentosMapeamento[areaIndex].tipologias[tipologiaIndex].categorias.push({
+              categoriaMapeamento: features[i].properties.categoriaMapeamento,
+              elementos: [features[i]]
+            });
+          } else {
+            investimentosMapeamento[areaIndex].tipologias[tipologiaIndex].categorias[categoriaIndex].elementos.push(features[i]);
+          }
+        }
+      }
+    }
+    return investimentosMapeamento
+  }
+
+
+  criarIconPersonalizado(feature: any) {
+    const BASE_CAMINHO_IMAGEM = 'assets/icones_novos';
+    const nameIcon = FeatureUtils.getIconPath(feature, BASE_CAMINHO_IMAGEM);
+    const myIcon = L.icon({
+      iconUrl: nameIcon,
+      iconSize: [35, 35],
+      shadowSize: [35, 20],
+      iconAnchor: [12, 12],
+      shadowAnchor: [12, 6],
+      popupAnchor: [0, 0]
+    });
+    return myIcon;
+  }
+
+  criarEstruturaArvore(investimentosMapeamento: any) {
+    this.investimentos_estrutura = {
+      label: ' INVESTIMENTOS',
+      selectAllCheckbox: 'true',
+      children: [] as Array<{ label: string; selectAllCheckbox: boolean; children: Array<any> }>
+    };
+
+    investimentosMapeamento.forEach((area: any) => {
+      let areaNode = {
+        label: ' ' + area.areaMapeamento,
+        selectAllCheckbox: true,
+        children: [] as Array<{ label: string; selectAllCheckbox: boolean; children: Array<any> }>
+
+      };
+
+      area.tipologias.forEach((tipologia: any) => {
+        let tipologiaNode = {
+          label: ' ' + tipologia.tipologiaMapeamento,
+          selectAllCheckbox: true,
+          children: [] as Array<{ label: string; selectAllCheckbox: boolean; children: Array<any> }>
+
+        };
+        areaNode.children.push(tipologiaNode);
+
+
+        tipologia.categorias.forEach((categoria: any) => {
+          let categoriaNode = {
+            label: ' ' + categoria.categoriaMapeamento,
+            selectAllCheckbox: true,
+            children: [] as Array<{ label: string; selectAllCheckbox: boolean; children: Array<any> }>
+
+          };
+
+          categoria.elementos.forEach((elemento: any) => {
+            let label = ' ' + elemento.properties.categoriaMapeamento
+            let cordenadas = elemento.geometry.coordinates;
+            let type = elemento.geometry.type;
+            let newElement: any;
+
+            if (type == "Point") {
+              let myIcon = this.criarIconPersonalizado(elemento);
+              const marker = L.marker([cordenadas[1], cordenadas[0]], { icon: myIcon });
+              FeatureUtils.customBindPopup(elemento, marker);
+              const objetoMarcadorFeature = {
+                marcador: marker,
+                feature: elemento
+              };
+              this.vetorMaker.push(objetoMarcadorFeature);
+              newElement = { label: label, layer: marker };
+            }
+            tipologiaNode.children.push(newElement);
+
+          })
+
+        })
+      })
+      this.investimentos_estrutura.children.push(areaNode);
+    })
   }
 
   filtrarCamadasPorInput() {
